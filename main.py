79ce4f6b87175f1598e703dc235ade6135533f31
NicolasKsmt <nicolas080501@gmail.com>
2023-03-13 07:58:42 -0300

    Subtle changes to the main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from config import *
 path = "E:\Tesseract\Tesseract.exe"
 pytesseract.pytesseract.tesseract_cmd = path
 
+<<<<<<< Updated upstream
 # dica : 'which tesseract' no terminal para descobrir o path do tesseract no linux e 'where tesseract' no windows.
 
 # Path default do tesseract no Linux (Ubuntu): r'/usr/bin/tesseract'
@@ -27,22 +28,37 @@ gray_image = getGray(image) # Converte a imagem para escala de cinza
 gauss_image = getGaussian(gray_image, 3, 3) # Aplica o desfoque Gaussiano na imagem
 thresh_image = getThresholding(gauss_image) # Aplica um threshold para transformar a imagem em binária
 norm_image = getNormalization(thresh_image) # Aplica a normalização na imagem
+=======
+image = cv2.imread("motiv.png")
+#showImage(image)
+#image_copy = bugFix(image)
 
-showImage(norm_image)
+gray_image = getGray(image)
+gauss_image = getGaussian(gray_image, 3, 3)
+thresh_image = getThresholding(gauss_image)
+norm_image = getNormalization(thresh_image)
+erode_image = getErosion(norm_image)
+>>>>>>> Stashed changes
 
-boxedImage = getBoxes(image)
+showImage(erode_image)
+
+boxedImage = getBoxes(erode_image)
 showImage(boxedImage)
 
+<<<<<<< Updated upstream
 # Transforma a imagem em texto
 
 text = pytesseract.image_to_string(norm_image, lang=language.LANG)
+=======
+text = pytesseract.image_to_string(erode_image, lang=language.LANG)
+>>>>>>> Stashed changes
 with open("text.txt", "w") as folder:
     folder.write(text)
 
-text_num = pytesseract.image_to_string(norm_image, config=custom_config_num)
+text_num = pytesseract.image_to_string(erode_image, config=custom_config_num)
 with open("text_num.txt", "w") as folder:
     folder.write(text_num)
 
-text_wl = pytesseract.image_to_string(norm_image, config=custom_config_wl)
+text_wl = pytesseract.image_to_string(erode_image, config=custom_config_wl)
 with open("text_wl.txt", "w") as folder:
     folder.write(text_wl)
